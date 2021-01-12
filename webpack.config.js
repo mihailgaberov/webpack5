e066d7bbea6c1158be924ec37443e59034179fff
Mihail Gaberov <mihail.gaberov@sequel.com>
2021-01-12 13:54:59 +0100

    :sparkles: Enabled HMR.

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
     devtool: 'inline-source-map',
     devServer: {
         contentBase: './dist',
+        hot: true,
     },
     plugins: [
         new CleanWebpackPlugin({cleanStaleWebpackAssets: false }),
