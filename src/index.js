4529004274aaa5dd7174ecbadb223fe29e0a39a4
Mihail Gaberov <mihail.gaberov@sequel.com>
2021-01-12 13:06:27 +0100

    :sparkles: Use dynamic import with async function.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import _ from 'lodash';
-
-function component() {
+async function getComponent() {
     const element = document.createElement('div');
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    const { default: _ } = await import('lodash');
+
+    element.innerHTML = _.join(['Hello', 'webpack', ' ']);
+
     return element;
 }
 
-document.body.appendChild(component());
+getComponent().then((component) => {
+    document.body.appendChild(component);
+});
